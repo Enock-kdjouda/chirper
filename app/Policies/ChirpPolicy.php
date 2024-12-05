ca6fa2f284af36ac7115a39563e085ed7a3efbf6
enock <enockdjoudas@gmail.com>
2024-12-05 15:16:40 +0100

    Deleting Chirps

--- app/Policies/ChirpPolicy.php
+++ app/Policies/ChirpPolicy.php
@@ -45,7 +45,8 @@ class ChirpPolicy
      */
     public function delete(User $user, Chirp $chirp): bool
     {
-        return false;
+        return $this->update($user, $chirp);
+       
     }
 
     /**
