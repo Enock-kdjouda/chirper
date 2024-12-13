7195d3361395389aa50b53941ad7f5fa6fcb986a
enock <enockdjoudas@gmail.com>
2024-12-13 22:18:42 +0100

    test de Exercice 4

--- tests/Feature/ChirpTest.php
+++ tests/Feature/ChirpTest.php
@@ -81,6 +81,24 @@ class ChirpTest extends TestCase
     }
 }
 
+// Exercice 4
+
+public function test_un_utilisateur_peut_modifier_son_chirp()
+    {
+    $utilisateur = User::factory()->create();
+    $chirp = Chirp::factory()->create(['user_id' => $utilisateur->id]);
+    $this->actingAs($utilisateur);
+    $reponse = $this->put("/chirps/{$chirp->id}", [
+    'message' => 'Chirp modifié'
+    ]);
+
+    $reponse->assertStatus(302);
+    // Vérifie si le chirp existe dans la base de donnée.
+    $this->assertDatabaseHas('chirps', [
+    'id' => $chirp->id,
+    'message' => 'Chirp modifié',
+    ]);
+    }
 
 
 }
